3b03801772832fe9c224d49a49dc5acda1277924
Kamlogogi <149802278+Kamlogogi@users.noreply.github.com>
2023-11-07 13:00:33 +0000

    add pics

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Kamilu Oluwafemi Lawal",
+  title: "PhD in Power Systems and Electric Machines",
+  email: "kolawal@atbu.edu.ng",
+  gitHub: "Kamlogogi",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "lawal-kamilu-o-rengr-mnse-2b420b45",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
